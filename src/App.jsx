f624be0ade488355e0be0f0171036f9ff82e978b
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-03-03 20:58:33 -0500

    Header component added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
 
 
 const App = () => {
   return (
       <Router>
+        <Header />
         <Routes>
           <Route path='/' element={<HomePage />} />
         </Routes>
